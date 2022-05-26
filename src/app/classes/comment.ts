14a90756741ef1bc6e3f66b57e9cad5d2f0b198d
severine <you@example.com>
2022-05-26 18:29:51 +0200

    Add name on comments + add send comment

---
+++ src/app/classes/comment.ts
@@ -0,0 +1,10 @@
+export class Comments {
+  constructor(
+  public id?: number,
+  public texte?: string,
+  public pouce?: number,
+  public createdAt?: Date,
+  public id_1?: number,
+  public id_2?: number,
+  ){}
+}
